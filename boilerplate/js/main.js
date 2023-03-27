47277b9840c65c0e9c1f0331bfab77971e5f2973
felix018jiang <felix018jiang@163.com>
2023-03-26 20:52:00 -0500

    update

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,86 +1,31 @@
-// Add all scripts to the JS folder
-//execute script when window is loaded
-window.onload = function () {
-    //Example 1.1 line 3...container block
+window.onload = function(){
+
+	//SVG dimension variables
+    var w = 900, h = 500;
+
+    //Example 1.5 line 1...container block
     var container = d3.select("body") //get the <body> element from the DOM
         .append("svg") //put a new svg in the body
-    var container = d3.select("body") //get the <body> element from the DOM
+        .attr("width", w) //assign the width
+        .attr("height", h) //assign the height
+        .attr("class", "container") //assign a class name
+        .style("background-color", "rgba(0,0,0,0.2)"); //svg background color
 
-};
-//SVG dimension variables
-var w = 900, h = 500;
+    //Example 1.8 line 1...innerRect block
+    var innerRect = container.append("rect")
+        .datum(400) //a single value is a DATUM
+        .attr("width", function(d){ //rectangle width
+            return d * 2; //400 * 2 = 800
+        })
+        .attr("height", function(d){ //rectangle height
+            return d; //400
+        })
+        .attr("class", "innerRect") //class name
+        .attr("x", 50) //position from left on the x (horizontal) axis
+        .attr("y", 50) //position from top on the y (vertical) axis
+        .style("fill", "#FFFFFF"); //fill color
 
-
-//Example 1.3 line 4...container block
-var container = d3.select("body") //get the <body> element from the DOM
-    .append("svg") //put a new svg in the body
-    .attr("width", w) //assign the width
-    .attr("height", h) //assign the height
-    .attr("class", "container") //assign a class name
-    .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
-
-//Example 1.8 line 1...innerRect block
-var innerRect = container.append("rect")
-    .datum(400) //a single value is a DATUM
-    .attr("width", function (d) { //rectangle width
-        return d * 2; //400 * 2 = 800
-    })
-    .attr("height", function (d) { //rectangle height
-        return d; //400
-    })
-    .attr("class", "innerRect") //class name
-    .attr("x", 50) //position from left on the x (horizontal) axis
-    .attr("y", 50) //position from top on the y (vertical) axis
-    .style("fill", "#FFFFFF"); //fill color
-
-var numbersArray = [1, 2, 3];
-
-var stringsArray = ["one", "two", "three"];
-
-var colorsArray = ["#F00", "#0F0", "#00F"];
-
-var objectsArray = [
-    {
-        city: 'Madison',
-        population: 233209
-    },
-    {
-        city: 'Milwaukee',
-        population: 594833
-    },
-    {
-        city: 'Green Bay',
-        population: 104057
-    }
-];
-
-var arraysArray = [
-    ['Madison', 23209],
-    ['Milwaukee', 593833],
-    ['Green Bay', 104057]
-];
-
-//Example 2.5 line 1
-var dataArray = [10, 20, 30, 40, 50];
-
-var circles = container.selectAll(".circles") //but wait--there are no circles yet!
-    .data(dataArray) //here we feed in an array
-    .enter() //one of the great mysteries of the universe
-    .append("circle") //add a circle for each datum
-    .attr("class", "circles") //apply a class name to all circles
-    .attr("r", function (d, i) { //circle radius
-        console.log("d:", d, "i:", i); //let's take a look at d and i
-        return d;
-    })
-    .attr("cx", function (d, i) { //x coordinate
-        return 70 + (i * 180);
-    })
-    .attr("cy", function (d) { //y coordinate
-        return 450 - (d * 5);
-    });
-
-    // using the city pop document to create circles
-        var cityPop = [
+    var cityPop = [
         { 
             city: 'Madison',
             population: 233209
@@ -99,6 +44,41 @@ var circles = container.selectAll(".circles") //but wait--there are no circles y
         }
     ];
 
+    //above Example 2.8 line 20
+    var x = d3.scaleLinear() //create the scale
+        .range([90, 810]) //output min and max
+        .domain([0, 3]); //input min and max
+
+    //above Example 2.8 line 20
+    //find the minimum value of the array
+    var minPop = d3.min(cityPop, function(d){
+        return d.population;
+    });
+
+    //find the maximum value of the array
+    var maxPop = d3.max(cityPop, function(d){
+        return d.population;
+    });
+
+    //scale for circles center y coordinate
+    var y = d3.scaleLinear()
+        .range([440, 95])
+        .domain([
+            minPop,
+            maxPop
+        ]);
+
+    //color scale generator 
+    var color = d3.scaleLinear()
+        .range([
+            "#FDBE85",
+            "#D94701"
+        ])
+        .domain([
+            minPop, 
+            maxPop
+        ]);
+
     //Example 2.6 line 3
     var circles = container.selectAll(".circles") //create an empty selection
         .data(cityPop) //here we feed in an array
@@ -115,9 +95,28 @@ var circles = container.selectAll(".circles") //but wait--there are no circles y
         })
         .attr("cx", function(d, i){
             //use the index to place each circle horizontally
-            return 90 + (i * 180);
+            return x(i);
         })
         .attr("cy", function(d){
             //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
-            return 450 - (d.population * 0.0005);
-        });
+            return y(d.population);
+        })
+        .style("fill", function(d, i){ //add a fill based on the color scale generator
+            return color(d.population);
+        })
+        .style("stroke", "#000"); //black circle stroke
+
+        //add axis
+        var yAxis = d3.axisLeft(y);
+
+        //create axis g element and add axis
+        var axis = container.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(50, 0)")
+                .call(yAxis);
+
+
+
+
+
+};
