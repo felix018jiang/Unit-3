ecb692bd85cdc5815cfa9c2d0fd1d08379b57a07
felix018jiang <felix018jiang@163.com>
2023-04-06 18:19:37 -0500

    update

--- boilerplate/js/main1.js
+++ boilerplate/js/main1.js
@@ -132,3 +132,32 @@ window.onload = function () {
 
 
 };
+//----------------------
+// window.onload = setMap();
+
+
+// //begin script when window loads
+// window.onload = setMap();
+
+// //Example 1.3 line 4...set up choropleth map
+// function setMap() {
+//     //use Promise.all to parallelize asynchronous data loading
+
+//     var promises = [
+//         d3.csv("data/lab2_data.csv"),
+//         d3.json("data/north_america.topojson"),
+//         d3.json("data/State_Boundaries.topojson"),
+//     ];
+
+//     Promise.all(promises).then(callback);
+
+//     function callback(data) {
+//         var csvData = data[0];
+//         na = data[1];
+//         us = data[2];
+//         var naCountries = topojson.feature(na, na.objects.State_Boundaries),
+//             usRegions = topojson.feature(us, us.objects.State_Boundaries);
+//     }
+// }
+// console.log(usRegions)
+//-----------
