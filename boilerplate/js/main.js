0db6659fef5e1557eae52d988bcf52e48b9f3328
felix018jiang <felix018jiang@163.com>
2023-04-08 16:44:53 -0500

    update

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,37 +1,15 @@
+//usregion
 //begin script when window loads
 window.onload = setMap();
-
 //Example 1.3 line 4...set up choropleth map
 function setMap() {
     //map frame dimensions
-    var width = 960,
-        height = 460;
-
-    //create new svg container for the map
-    var map = d3
-        .select("body")
-        .append("svg")
-        .attr("class", "map")
-        .attr("width", width)
-        .attr("height", height);
-
-    //create Albers equal area conic projection centered on France
-    var projection = d3
-        .geoAlbers()
-        .center([0, 46.2])
-        .rotate([-2, 0, 0])
-        .parallels([43, 62])
-        .scale(2500)
-        .translate([width / 2, height / 2]);
-
-    var path = d3.geoPath().projection(projection);
-
-    //use Promise.all to parallelize asynchronous data loading
     var promises = [
         d3.csv("data/lab2_data.csv"),
         d3.json("data/north_america.topojson"),
         d3.json("data/State_Boundaries.topojson"),
     ];
+
     Promise.all(promises).then(callback);
 
     function callback(data) {
@@ -39,37 +17,27 @@ function setMap() {
             na = data[1],
             us = data[2];
 
-        //translate na TopoJSON
-        var naCountries = topojson.feature(na, na.objects.State_Boundaries),
-            usRegions = topojson.feature(us, us.objects.State_Boundaries);
+            console.log(na)
+            console.log(na)
+        var width = 960,
+            height = 460;
 
-        var graticule = d3.geoGraticule().step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+        var map = d3
+        .select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
 
-        //create graticule background
-        var gratBackground = map
-            .append("path")
-            .datum(graticule.outline()) //bind graticule background
-            .attr("class", "gratBackground") //assign class for styling
-            .attr("d", path); //project graticule
+        var projection = d3
+        .geoAlbers()
+        .center([0, 46.2])
+        .rotate([-2, 0, 0])
+        .parallels([43, 62])
+        .scale(2500)
+        .translate([width / 2, height / 2]);
 
-        //create graticule lines
-        var gratLines = map
-            .selectAll(".gratLines") //select graticule elements that will be created
-            .data(graticule.lines()) //bind graticule lines to each element to be created
-            .enter() //create an element for each datum
-            .append("path") //append each element to the svg as a path element
-            .attr("class", "gratLines") //assign class for styling
-            .attr("d", path); //project graticule lines
-
-        //add Europe countries to map
-        var countries = map
-            .append("path")
-            .datum(naCountries)
-            .attr("class", "countries")
-            .attr("d", path);
-
-        //add France regions to map
-        var regions = map.selectAll()
+        var regions = map
             .selectAll(".regions")
             .data(usRegions)
             .enter()
@@ -78,17 +46,20 @@ function setMap() {
                 return "regions " + d.properties.adm1_code;
             })
             .attr("d", path);
-        //translate europe TopoJSON
+
+        //translate na TopoJSON
         var naCountries = topojson.feature(na, na.objects.State_Boundaries),
             usRegions = topojson.feature(us, us.objects.State_Boundaries);
 
         var countries = map.append("path")
-            .datum(naCountries)
+            .datum(usRegions)
             .attr("class", "countries")
             .attr("d", path);
 
-        //add France regions to map
-        var regions = map.selectAll(".regions")
+
+        //add Europe countries to map
+        var regions = map
+            .selectAll(".regions")
             .data(usRegions)
             .enter()
             .append("path")
@@ -96,46 +67,24 @@ function setMap() {
                 return "regions " + d.properties.adm1_code;
             })
             .attr("d", path);
+
+
     }
-    //Example 1.4 line 1...set up choropleth map
-    function setMap() {
 
-        //map frame dimensions
-        var width = 960,
-            height = 460;
+    
 
-        //create new svg container for the map
-        var map = d3.select("body")
-            .append("svg")
-            .attr("class", "map")
-            .attr("width", width)
-            .attr("height", height);
+    //create new svg container for the map
+    
 
-        //create Albers equal area conic projection centered on France
-        var projection = d3.geoAlbers()
-            .center([0, 46.2])
-            .rotate([-2, 0, 0])
-            .parallels([43, 62])
-            .scale(2500)
-            .translate([width / 2, height / 2]);
+    //create Albers equal area conic projection centered on France
+    
+    // var path = d3.geoPath().projection(projection);
 
-        //use Promise.all to parallelize asynchronous data loading
-        var promises = [];
-        promises.push(d3.csv("data/lab2_data.csv")); //load attributes from csv    
-        promises.push(d3.json("data/north_america.topojson")); //load background spatial data    
-        promises.push(d3.json("data/State_Boundaries.topojson")); //load choropleth spatial data    
-        Promise.all(promises).then(callback);
-    }
-    //Example 2.1 line 15...create Albers equal area conic projection centered on France
-    var projection = d3.geoAlbers()
-        .center([0, 46.2])
-        .rotate([-2, 0])
-        .parallels([43, 62])
-        .scale(2500)
-        .translate([width / 2, height / 2]);
+    //use Promise.all to parallelize asynchronous data loading
 
-    var path = d3.geoPath()
-        .projection(projection);
+
+    
+
 
 
 }
